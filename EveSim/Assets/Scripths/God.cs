ac5c04cc3f098a55cc6550147ea25ab429a333c1
Jurmungandur <31280587+Jurmungandur@users.noreply.github.com>
2019-05-02 21:48:48 +0200

    Basic Movment

--- EveSim/Assets/Scripths/God.cs
+++ EveSim/Assets/Scripths/God.cs
@@ -6,53 +6,93 @@ public class God : MonoBehaviour
 {
     public GameObject Unit;
     public GameObject Food;
+    public float sizeOfMap;
+    public int DayNr = -2;
     public int UnitCount;
     public int FoodPerDay;
     public int FoodChange;
+    public GameObject[] unitsInGame;
     private int FoodToday;
     private int randomNumber = 0;
-    
+    public int activeUnits;
+    private bool settingUpDay = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        activeUnits = 1;
         for (int i = 0; i < UnitCount; i++)
         {
             randomNumber = Random.Range(1, 5);
             if (randomNumber == 1)
             {
-                var newUnit = Instantiate(Unit, new Vector3(-5, 1, Random.Range(-5, 5)), transform.rotation);
-                newUnit.transform.parent = gameObject.transform;
+                var newUnit = Instantiate(Unit, new Vector3(-sizeOfMap, 1.1f, Random.Range(-sizeOfMap, sizeOfMap)), transform.rotation);
+                newUnit.GetComponent<UnitBehavour>().speed = 1;
+                newUnit.GetComponent<UnitBehavour>().enegy = 200;
             }
             if (randomNumber == 2)
             {
-                var newUnit = Instantiate(Unit, new Vector3(Random.Range(-5, 5), 1, -5), transform.rotation);
-                newUnit.transform.parent = gameObject.transform;
+                var newUnit = Instantiate(Unit, new Vector3(Random.Range(-sizeOfMap, sizeOfMap), 1.1f, -sizeOfMap), transform.rotation);
+                newUnit.GetComponent<UnitBehavour>().speed = 1;
+                newUnit.GetComponent<UnitBehavour>().enegy = 200;
             }
             if (randomNumber == 3)
             {
-                var newUnit = Instantiate(Unit, new Vector3(5, 1, Random.Range(-5, 5)), transform.rotation);
-                newUnit.transform.parent = gameObject.transform;
+                var newUnit = Instantiate(Unit, new Vector3(sizeOfMap, 1.1f, Random.Range(-sizeOfMap, sizeOfMap)), transform.rotation);
+                newUnit.GetComponent<UnitBehavour>().speed = 1;
+                newUnit.GetComponent<UnitBehavour>().enegy = 200;
             }
             if (randomNumber == 4)
             {
-                var newUnit = Instantiate(Unit, new Vector3(Random.Range(-5, 5), 1, 5), transform.rotation);
-                newUnit.transform.parent = gameObject.transform;
+                var newUnit = Instantiate(Unit, new Vector3(Random.Range(-sizeOfMap, sizeOfMap), 1.1f, sizeOfMap), transform.rotation);
+                newUnit.GetComponent<UnitBehavour>().speed = 1;
+                newUnit.GetComponent<UnitBehavour>().enegy = 200;
             }
+            NewDay(DayNr);
+            settingUpDay = true;
         }
-        SpawnFood();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        unitsInGame = GameObject.FindGameObjectsWithTag("Unit");
+        activeUnits = unitsInGame.Length;
+        if (activeUnits <= 0) {
+            if(settingUpDay == false)
+            {
+                NewDay(DayNr);
+                settingUpDay = true;
+            }
+        }
+    }
+
+    void NewDay(int CurrentDay) {
+
+        float totalspeedtoday = 0;
+        float avragespeedtoday = 0;
+        SpawnFood();
+        unitsInGame = GameObject.FindGameObjectsWithTag("Unit");
+        print("Day " + CurrentDay);
+        foreach (GameObject Unit in unitsInGame)
+        {
+            totalspeedtoday += Unit.GetComponent<UnitBehavour>().speed;
+        }
+        avragespeedtoday = totalspeedtoday / unitsInGame.Length;
+        print("Avrage speed: " + avragespeedtoday);
+        foreach (GameObject Unit in unitsInGame)
+        {
+            Unit.GetComponent<UnitBehavour>().Active();
+        }
+        settingUpDay = false;
+        DayNr++;
     }
 
     void SpawnFood() {
-        FoodToday = FoodPerDay + Random.RandomRange(-FoodChange, FoodChange);
+        FoodToday = FoodPerDay + Random.Range(-FoodChange, FoodChange);
         for (int y = 0; y < FoodToday; y++)
         {
-            Instantiate(Food, new Vector3(Random.RandomRange(-4, 4), 0.5f, Random.RandomRange(-4, 4)), transform.rotation);
+            Instantiate(Food, new Vector3(Random.Range(-sizeOfMap, sizeOfMap), 0.5f, Random.Range(-sizeOfMap, sizeOfMap)), transform.rotation);
         }
     }
 }
--- EveSim/Assets/Scripths/UnitBehavour.cs
+++ EveSim/Assets/Scripths/UnitBehavour.cs
@@ -5,4 +5,45 @@ using UnityEngine;
 public class UnitBehavour : MonoBehaviour
 {
     public float speed;
+    public GameObject god;
+    public float enegy = 200;
+    public Rigidbody rb;
+    private float rotateBy = 0;
+    private float t = 0.0f;
+    public bool isActive = false;
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        transform.LookAt(new Vector3(0, 1, 0));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isActive == true) {
+            if (enegy > 0)
+            {
+                t = t + Time.deltaTime;
+                if (t > 0.75f)
+                {
+                    rotateBy = Random.Range(-5, 5);
+                    t = 0;
+                }
+                transform.Rotate(new Vector3(0, rotateBy, 0), Space.Self);
+                rb.velocity = transform.forward * speed * 2;
+                enegy -= speed;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void Active() {
+        transform.LookAt(new Vector3(0, 1, 0));
+        enegy = 300;
+        isActive = true;
+    }
 }
